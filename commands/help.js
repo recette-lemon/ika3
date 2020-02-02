61fe50aec3ec7c8e0fab3918106e77d751d9e24b
eiko <me@eiko.cc>
2020-02-02 15:23:27 +0000

    redid help command to point to a webpage

--- commands/help.js
+++ commands/help.js
@@ -4,63 +4,23 @@ module.exports = {
 	description: "Help message",
 	category: "misc",
 	arguments: {
-		positional: ["section"],
+		positional: [],
 		args: []
 	},
 	func: func
 };
 
 function func(message, args){
-	let section = args._[0];
+	
 	let embed = new Discord.RichEmbed({
 		thumbnail: {
 			url: "https://i.imgur.com/gv4Itmg.png"
 		},
 		color: Config.embedColour,
-		title: "Help for " + Bot.user.tag,
-		description: "Run `" + Config.trigger + "help <section>` to get each category of commands",
 	});
-	switch(section){
-		case "general":
-		for(cmd in Commands){
-			if(Commands[cmd].category == "general") embed.addField(cmd, Commands[cmd].description);
-		}
-		break;
-		case "lewd":
-		for(cmd in Commands){
-			if(Commands[cmd].category == "lewd") embed.addField(cmd, Commands[cmd].description);
-		}
-		break;
-		case "meme":
-		for(cmd in Commands){
-			if(Commands[cmd].category == "meme") embed.addField(cmd, Commands[cmd].description);
-		}
-		break;
-		case "music":
-		for(cmd in Commands){
-			if(Commands[cmd].category == "music") embed.addField(cmd, Commands[cmd].description);
-		}
-		break;
-		case "image":
-		for(cmd in Commands){
-			if(Commands[cmd].category == "image") embed.addField(cmd, Commands[cmd].description);
-		}
-		break;
-		case "misc":
-		for(cmd in Commands){
-			if(Commands[cmd].category == "misc") embed.addField(cmd, Commands[cmd].description);
-		}
-		break;
-		default:
-		case undefined:
-		embed.addField('General', "Just general commands.");
-		embed.addField('Lewd', 'Hot, sexy, anime people.');
-		embed.addField('Meme', "Meme and joke commands.");
-		embed.addField('Music', "Music bot commands.")
-		embed.addField('Image', "Image manipulation commands.");
-		embed.addField('Misc', "Miscellaneous commands.");
-		break;
-	}
+
+	embed.addField("Help Page", "[Ika.Sylvie.Moe/Help](https://ika.sylvie.moe/help)");
+
 	message.channel.send({embed});
 	for(cmd in Commands) console.log(Commands[cmd]);
 }
