da2229e40b51eb9ccb9e1e61c38cdb878b049a1b
eiko <me@eiko.cc>
2020-02-02 15:35:06 +0000

    scaled down roll command

--- commands/roll.js
+++ commands/roll.js
@@ -10,6 +10,8 @@ module.exports = {
 	func: func
 };
 
+let congrats = ['Singles', 'Dubs', 'Trips!', 'Quads!!', 'Quints!!!', 'Holy shit ur a god!!!1!1!'];
+
 function func(message, args){
 	let embed = new Discord.RichEmbed({
 		color: Config.embedColour,
@@ -19,20 +21,16 @@ function func(message, args){
 		}
 	});
 
-	let mes = args._.join(" ").split("`").join("").split("\n")[0];
-	if(mes.length > 250) mes = mes.slice(0, 250) + '...';
-	if(message.attachments.first()) embed.setImage(message.attachments.first().url);
-	if(message.content.toLowerCase().split(" ")[0] == (Config.trigger + "reroll")){
-		embed.setTitle("Reroll it!");
-		if(mes) embed.addField("Rerolling for", mes);
-	} else {
-		embed.setTitle("Roll it!");
-		if(mes) embed.addField("Rolling for", mes);
-	}
+	let mes = args._.join(" ");
+	if(message.attachments.first())
+		embed.setImage(message.attachments.first().url);
+	if(mes)
+		embed.description = mes.slice(0, 250);
+	embed.setTitle("Roll it!");
 
-	let roll = (Math.floor(Math.random() * 999999999) + 1);
+	let roll = Math.floor(Math.random() * 999999999);
 	let streak = roll.toString().match(/(.)\1*$/)[0].length;
-	let congrats = ['Singles', 'Dubs', 'Trips!', 'Quads!!', 'Quints!!!', 'Holy shit ur a god!!!1!1!']
+
 	embed.addField((streak < 6 ? congrats[streak - 1] : congrats[5]), roll);
 	message.channel.send({embed});
 }
