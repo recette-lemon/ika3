c458a92e9f627e8ae0e83efb9c642e8ee074ee4a
eiko <me@eiko.cc>
2020-02-26 10:09:05 +0000

    redirects if theres more than one word, and results seem to be the same in testing, so ill just revert

--- commands/search.js
+++ commands/search.js
@@ -1,53 +1,52 @@
 module.exports = {
-	name: "Search",
-	triggers: ["search", "g"],
-	description: "Search using Searx.me.",
-	category: "search",
-	arguments: {
-		positional: ["terms"],
-		args: []
-	},
-	func: func
+    name: "Search",
+    triggers: ["search", "g"],
+    description: "Search using Searx.me.",
+    category: "search",
+    arguments: {
+        positional: ["terms"],
+        args: []
+    },
+    func: func
 };
-
+ 
 const Request = require("request");
-const baseURL = "https://duckduckgo.com/?q=";
-
+const URL = "https://duckduckgo.com/lite/";
+ 
 function func(message, args){
-	let string = args._.join(" ");
-
-	if(!string)
-		return message.reply("Need something to search for, buddy.");
-
-	let url = baseURL + encodeURIComponent(string);
-
-	Request.get(url, (err, res, bod) => {
-		if(!bod || err)
-			return message.reply("Ok, something didn't work.");
-
-		let d = "https://duckduckgo.com" + bod.match(/\/d\.js.+(?='\);DDH)/)[0];
-
-		Request.get(d, (err, res, bod) => {
-
-			let results = JSON.parse(bod.match(/(?<=nrn\('d',).+\).+(?=\);DDG\.deep\.bing)/)[0]);
-
-			if(!results)
-				return message.reply("Nothing found.");
-
-			message.channel.send("Result 1 of "+results.length+" "+results[0].u).then(mes=>{
-				let controls = new Utility.MessageControls(mes, message.author),
-					index = 0;
-
-				controls.on("reaction", r => {
-					if(r.n === 0 && results[index-1])
-						index--;
-					else if(r.n === 1 && results[index+1])
-						index++;
-					else return;
-
-					mes.edit("Result "+(index+1)+" of "+results.length+" "+results[index].u);
-				});
-			});
-		});
-	});
+    let string = args._.join(" ");
+ 
+    if(!string)
+        return message.reply("Need something to search for, buddy.");
+ 
+    Request.post(URL, {
+        form: {
+            q: encodeURIComponent(string),
+            kl: "wt-wt"
+        }
+    }, (err, ress, bod) => {
+ 
+        if(!bod || err)
+            return message.reply("Ok, something didn't work.");
+ 
+        let res = bod.match(/(?<=(href=["']))(.+)(?=(["'] class=["']result))/g);
+ 
+        if(!res)
+            return message.reply("Nothing found.");
+ 
+        message.channel.send("Result 1 of "+res.length+" "+res[0]).then(mes=>{
+            let controls = new Utility.MessageControls(mes, message.author),
+                index = 0;
+ 
+            controls.on("reaction", r => {
+                if(r.n === 0 && res[index-1])
+                    index--;
+                else if(r.n === 1 && res[index+1])
+                    index++;
+                else return;
+ 
+                mes.edit("Result "+(index+1)+" of "+res.length+" "+res[index]);
+            });
+        });
+    });
 }
