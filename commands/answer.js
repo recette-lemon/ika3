be00d29ac82c3d9a46a7823d6cfcb82e3a6b1ffa
eiko <me@eiko.cc>
2020-03-08 15:23:39 +0000

    answer is properly deterministic now

--- commands/answer.js
+++ commands/answer.js
@@ -10,6 +10,9 @@ module.exports = {
 	func: func
 };
 
-function func(message){
-	message.reply(["Yes.", "No."][parseInt(message.id[message.id.length-1])%2]);
+function func(message, args){
+	let q = args._.join("").toLowerCase().match(/[a-z]/g);
+	if(!q)
+		return message.reply("Nothing to answer.");
+	message.reply(["Yes.", "No."][q.map(c => {return c.charCodeAt(0)}).reduce((a,b) => {return a+b}) % 2]);
 }
