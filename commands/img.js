0c16cfb7cedc77a2dad838b72eefce869102f204
eiko <me@eiko.cc>
2020-04-29 19:17:24 +0000

    did the same for img

--- commands/img.js
+++ commands/img.js
@@ -1,34 +1,37 @@
 module.exports = {
 	name: "Img",
 	triggers: ["img"],
-	description: "Image search using DuckDuckGo.",
+	description: "Image search using Startpage (Google) or DuckDuckGo. Defaults to Startpage.",
 	category: "search",
 	arguments: {
-		positional: ["terms"]
+		positional: ["terms"],
+		flags: {
+			backend: {
+				duckduckgo: [false, "ddg", "d"],
+				startpage: [false, "sp", "s"]
+			}
+		}
 	},
 	func: func
 };
 
-var pageBase = "http://duckduckgo.com/?ia=images&iax=images&k5=1&kp=-2&q=";
-var jsonBase = "https://duckduckgo.com/i.js?l=us-en&o=json&vqd=VQD&f=,,,&p=-1&v7exp=a&q=";
+let pageBase = "http://duckduckgo.com/?ia=images&iax=images&k5=1&kp=-2&q=";
+let jsonBase = "https://duckduckgo.com/i.js?l=us-en&o=json&vqd=VQD&f=,,,&p=-1&v7exp=a&q=";
+let Request = require("request");
+let HTMLParse = require("node-html-parser").parse;
 
-function func(message, args){
-	let query = encodeURIComponent(args._.join(" "));
-	if(!query)
-		return message.reply("Gonna need something to search for.");
+function duckduckgo(message, string){
+	let query = encodeURIComponent(string);
 	
-	Utility.get(pageBase+query, {}, (perr, pres, pbod) => {
+	Request.get(pageBase+query, {}, (perr, pres, pbod) => {
 		if(perr || !pbod)
 			throw("nope");
 
-		let vqd = pbod.match(/&vqd=(.+)&p/)[1],
-			url = jsonBase.replace("VQD", vqd)+query;
-
+		let vqd = pbod.match(/&vqd=(.+)&p/)[1];
+		let url = jsonBase.replace("VQD", vqd)+query;
 
 		Utility.get(url, (jerr, jres, jbod) => {
-
 			let results = JSON.parse(jbod).results;
-			let index = 0;
 
 			if(!results[0])
 				return message.reply("Nothing found.");
@@ -49,8 +52,8 @@ function func(message, args){
 			});
 
 			message.channel.send({embed}).then(mes=>{
-				let controls = new Utility.MessageControls(mes, message.author),
-					index = 0;
+				let controls = new Utility.MessageControls(mes, message.author);
+				let index = 0;
 
 				controls.on("reaction", r => {
 					if(r.n === 0 && results[index-1])
@@ -70,4 +73,72 @@ function func(message, args){
 			});
 		});
 	});
+}
+
+function startpage(message, string){
+	Request.post("https://startpage.com/sp/search", {
+		headers: {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+			"Accept-Encoding": "identity" // no gzip thx
+		},
+		form: {
+			"language": "english",
+			"lui": "english",
+			"query": string,
+			"cat": "pics"
+		}
+	}, (err, res, bod) => {
+		if(err || !bod)
+			return message.reply("Something went wrong.");
+		let results = bod.match(/(?<=data-img-metadata=')(.+)(?=')/g);
+		if(!results)
+			return message.reply("No results.");
+		results = results.map(JSON.parse);
+
+		let embed = new Discord.RichEmbed({
+			title: HTMLParse(results[0].title).text,
+			author: {
+				name: results[0].displayUrl.split("/")[2],
+				url: results[0].displayUrl
+			},
+			image: {
+				url: results[0].clickUrl
+			},
+			color: Config.embedColour,
+			footer: {
+				text: "1 of "+results.length + " | " + results[0].width+"x"+results[0].height
+			}
+		});
+
+		message.channel.send({embed}).then(mes=>{
+			let controls = new Utility.MessageControls(mes, message.author);
+			let index = 0;
+
+			controls.on("reaction", r => {
+				if(r.n === 0 && results[index-1])
+					index--;
+				else if(r.n === 1 && results[index+1])
+					index++;
+				else return;
+
+				embed.author.name = results[index].displayUrl.split("/")[2];
+				embed.author.url = results[index].displayUrl;
+				embed.image.url = results[index].clickUrl;
+				embed.title = HTMLParse(results[index].title).text;
+				embed.footer.text = (index+1)+" of "+results.length + " | " + results[index].width+"x"+results[index].height;
+
+				mes.edit({embed});
+			});
+		});
+	});
+}
+
+let backends = {duckduckgo, startpage};
+
+function func(message, args){
+	let string = args._.join(" ");
+	if(!string)
+		return message.reply("Gonna need something to search for.");
+
+	backends[args.backend || "startpage"](message, string);
 }
