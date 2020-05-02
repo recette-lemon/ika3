acf5dda4d53155d214deef8b482d93013736ef81
eiko <me@eiko.cc>
2020-05-02 19:33:35 +0000

    forgot to add index.js

--- index.js
+++ index.js
@@ -4,25 +4,21 @@ global.Config = JSON.parse(require("fs").readFileSync("config.json"));
 global.Utility = require("./utility");
 
 global.Images = Utility.getImageLists();
-
 global.Commands = Utility.getCommands();
 
 global.Bot = new Discord.Client();
 Bot.login(Config.token);
 
-global.guildConfigs = null;
-global.DB = null;
-
 Bot.on("ready", () => {
-	console.log("ready");
+	console.log("Ready.");
+	Utility.checkMutes();
 	setInterval(Utility.statusRotate, 10000);
 	Utility.statusRotate();
-	Utility.initDB();
 });
 
 Bot.on("guildMemberAdd", (member) => {
-	if(guildConfigs[member.guild.id].mutes[member.id])
-		Utility.checkUserMute(guildConfigs[member.guild.id].mutes[member.id], member.guild.id, member.id);
+	if(Configs.get(member.guild.id).get("mutes").get(member.id))
+		Utility.checkUserMute(member.guild.id, member.id);
 });
 
 Bot.on("message", Utility.parseMessage);
