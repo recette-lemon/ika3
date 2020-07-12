1b9d7eab5647af2d82a1c086cc752e25138036f9
recette <recette@420blaze.it>
2020-07-12 12:20:08 +0000

    improved urban command

--- commands/urban.js
+++ commands/urban.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "Urban Dictionary",
 	triggers: ["urban", "ud"],
-	description: "Gets a definition from UD.",
+	description: "Gets definitions from UD.",
 	category: "search",
 	arguments: {
 		positional: ["term"],
@@ -16,11 +16,15 @@ function func(message, args){
 	let term = args._.join(" ");
 	if(!term)
 		return reply("Need something to define.");
-
 	let url = "http://www.urbandictionary.com/define.php?term="+encodeURIComponent(term);
-
 	Request.get(url, (err, res, bod) => {
-		let def = bod.match(/property="fb:app_id"><meta content\=\"(.+)" name="Descri/);
-		message.reply(def ? HTMLParse(def[1]).text : "Not found");
+		if(err || !bod)
+			return message.reply("Failed.");
+		let meanings = HTMLParse(bod).querySelectorAll(".meaning").map(r => r.text);
+		if(!meanings.length)
+			return message.reply("Nothing found.");
+		Utility.scrollControls(message, meanings, (res, posStr) => {
+			return `<${url}>\n${res.slice(0, 1500)+(res.length>1500?"...":"")}\n${posStr}`;
+		});
 	});
 }
