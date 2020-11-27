3d77b8842502ec2d7f3df73b7aa95d83a1c704d4
recette <recette@420blaze.it>
2020-11-27 21:38:33 +0000

    added jetmp4 as an alternative yt search backend, since invidious instances are being aids

--- commands/youtube.js
+++ commands/youtube.js
@@ -5,6 +5,12 @@ module.exports = {
 	category: "search",
 	arguments: {
 		positional: ["terms"],
+		flags: {
+			backend: {
+				invidious: [false, "i"],
+				jetmp4: [false, "j"]
+			}
+		}
 	},
 	func: func
 };
@@ -18,14 +24,31 @@ function output(res, posStr){
 	return posStr+" "+link+"\n"+res.description.split("\n")[0].replace(/(https?:\/\/\S+)/, "<$1>");
 }
 
+let Request = require("request");
+let HTMLParse = require("node-html-parser").parse;
+
 function func(message, args){
 	let terms = args._.join(" ");
 	if(!terms)
 		return message.reply("Need something to search for.");
-	Utility.searchYT(terms, res => {
-		if(!res[0])
-			return message.reply("Nothing found.");
-		Utility.scrollControls(message, res, output);
+	if(args.invidious){
+		return Utility.searchYT(terms, res => {
+			if(!res[0])
+				return message.reply("Nothing found.");
+			Utility.scrollControls(message, res, output);
+		});
+	}
+	let url = `https://jetmp4.com/video/${terms.replace(/\s+/g, "_").replace(/\W+/, "")}.html`;
+	Request.get(url, (err, res, bod) => {
+		let results = [];
+		for(let i of HTMLParse(bod).querySelectorAll(".d img")){
+			results.push({
+				type: "video",
+				videoId: i.getAttribute("data-src").split("/")[4],
+				description: ""
+			});
+		}
+		Utility.scrollControls(message, results, output);
 	});
 }
 
